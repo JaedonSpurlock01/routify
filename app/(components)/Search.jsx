576fc16f4e7176a1c1ae2dfe25d41533ba6b378e
JaedonSpurlock01 <112233517+JaedonSpurlock01@users.noreply.github.com>
2024-02-14 20:39:13 -0800

    bug: fixed import names

--- app/(components)/Search.jsx
+++ app/(components)/Search.jsx
@@ -3,7 +3,6 @@ import React, { useState, useContext } from "react";
 import { IoIosSearch } from "react-icons/io";
 import { RotatingLines, ProgressBar } from "react-loader-spinner";
 
-import jsonData from "@/lib/test-cities/san-diego.json";
 import { ThreeContext } from "@/lib/context/three.context";
 import request from "@/lib/services/request";
 
