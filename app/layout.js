fb800f04f477ea96bb5bf6fc9d6e0e5c1d9ca34d
JaedonSpurlock01 <112233517+JaedonSpurlock01@users.noreply.github.com>
2024-03-26 20:27:20 -0700

    feat: added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +20,9 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-[#121212]">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children} <Analytics />
+      </body>
     </html>
   );
 }
